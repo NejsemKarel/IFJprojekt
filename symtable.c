a2fc1219c52353c6ff8bb78f0bcfc79241daf1a7
Filip Petrasek <petrasek.filda@gmail.com>
2022-11-29 15:06:09 +0100

    symtable 3structs

--- symtable.c
+++ symtable.c
@@ -11,10 +11,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+
+
+
+
+// FUNCTION BTREE
+
+
 /*
 * Inicializace stromu
 */
-void btree_init(btree_node_t **tree)
+void btree_function_init(btree_function_t **tree)
 {
     *tree = NULL;
 }
@@ -23,11 +31,11 @@ void btree_init(btree_node_t **tree)
 /*
 * Vymazání celého stromu
 */
-void btree_dispose(btree_node_t **tree)
+void btree_function_dispose(btree_function_t **tree)
 {
     if((*tree) != NULL){
-    btree_dispose(&(*tree)->right);
-    btree_dispose(&(*tree)->left);
+    btree_function_dispose(&(*tree)->right);
+    btree_function_dispose(&(*tree)->left);
     free((*tree));
     (*tree) = NULL;
   }
@@ -37,11 +45,11 @@ void btree_dispose(btree_node_t **tree)
 /*
 * Přidání prvku do stromu
 */
-void btree_add(btree_node_t **tree, int order, char ident, char value)
+void btree_function_add(btree_function_t **tree, int order, char ident, char value)
 {
     if((*tree) == NULL)
     {
-        btree_node_t *element = malloc(sizeof(struct btree_node));
+        btree_function_t *element = malloc(sizeof(struct btree_function));
         if(element == NULL)
             return;
         element->order = order;
@@ -58,18 +66,18 @@ void btree_add(btree_node_t **tree, int order, char ident, char value)
     }
     else if((*tree)->order > order)
     {
-        btree_add(&((*tree)->left), order, ident, value);
+        btree_function_add(&((*tree)->left), order, ident, value);
     }
     else
     {
-        btree_add(&((*tree)->right), order, ident, value);
+        btree_function_add(&((*tree)->right), order, ident, value);
     }
 }
 
 /*
 * Vyhledá prvek v stromě
 */
-bool btree_search(btree_node_t *tree, int order, char ident, char value)
+bool btree_function_search(btree_function_t *tree, int order, char ident, char value)
 {
     if (tree == NULL)
     {
@@ -85,11 +93,202 @@ bool btree_search(btree_node_t *tree, int order, char ident, char value)
         }
         else if(tree->order > order)
         {
-            return btree_search(tree->left , order, ident, value);
+            return btree_function_search(tree->left , order, ident, value);
         }
         else
         {
-            return btree_search(tree->right, order, ident, value);
+            return btree_function_search(tree->right, order, ident, value);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// VARIABLE BTREE
+
+
+/*
+* Inicializace stromu
+*/
+void btree_variable_init(btree_variable_t **tree)
+{
+    *tree = NULL;
+}
+
+
+/*
+* Vymazání celého stromu
+*/
+void btree_variable_dispose(btree_variable_t **tree)
+{
+    if((*tree) != NULL){
+    btree_variable_dispose(&(*tree)->right);
+    btree_variable_dispose(&(*tree)->left);
+    free((*tree));
+    (*tree) = NULL;
+  }
+}
+
+
+/*
+* Přidání prvku do stromu
+*/
+void btree_variable_add(btree_variable_t **tree, int order, char ident, char value)
+{
+    if((*tree) == NULL)
+    {
+        btree_variable_t *element = malloc(sizeof(struct btree_variable));
+        if(element == NULL)
+            return;
+        element->order = order;
+        element->ident = ident;
+        element->value = value;
+        element->left = NULL;
+        element->right = NULL;
+        (*tree) = element;
+    }
+    else if((*tree)->order == order)
+    {
+        (*tree)->ident = ident;
+        (*tree)->value = value;
+    }
+    else if((*tree)->order > order)
+    {
+        btree_variable_add(&((*tree)->left), order, ident, value);
+    }
+    else
+    {
+        btree_variable_add(&((*tree)->right), order, ident, value);
+    }
+}
+
+/*
+* Vyhledá prvek v stromě
+*/
+bool btree_variable_search(btree_variable_t *tree, int order, char ident, char value)
+{
+    if (tree == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        if (tree->order == order)
+        {
+            ident = tree->ident;
+            value = tree->value;
+            return true;
+        }
+        else if(tree->order > order)
+        {
+            return btree_variable_search(tree->left , order, ident, value);
+        }
+        else
+        {
+            return btree_variable_search(tree->right, order, ident, value);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// PROGRAM BTREE
+
+/*
+* Inicializace stromu
+*/
+void btree_program_init(btree_program_t **tree)
+{
+    *tree = NULL;
+}
+
+
+/*
+* Vymazání celého stromu
+*/
+void btree_program_dispose(btree_program_t **tree)
+{
+    if((*tree) != NULL){
+    btree_program_dispose(&(*tree)->right);
+    btree_program_dispose(&(*tree)->left);
+    free((*tree));
+    (*tree) = NULL;
+  }
+}
+
+
+/*
+* Přidání prvku do stromu
+*/
+void btree_program_add(btree_program_t **tree, int order, char ident, char value)
+{
+    if((*tree) == NULL)
+    {
+        btree_program_t *element = malloc(sizeof(struct btree_program));
+        if(element == NULL)
+            return;
+        element->order = order;
+        element->ident = ident;
+        element->value = value;
+        element->left = NULL;
+        element->right = NULL;
+        (*tree) = element;
+    }
+    else if((*tree)->order == order)
+    {
+        (*tree)->ident = ident;
+        (*tree)->value = value;
+    }
+    else if((*tree)->order > order)
+    {
+        btree_program_add(&((*tree)->left), order, ident, value);
+    }
+    else
+    {
+        btree_program_add(&((*tree)->right), order, ident, value);
+    }
+}
+
+/*
+* Vyhledá prvek v stromě
+*/
+bool btree_program_search(btree_program_t *tree, int order, char ident, char value)
+{
+    if (tree == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        if (tree->order == order)
+        {
+            ident = tree->ident;
+            value = tree->value;
+            return true;
+        }
+        else if(tree->order > order)
+        {
+            return btree_program_search(tree->left , order, ident, value);
+        }
+        else
+        {
+            return btree_program_search(tree->right, order, ident, value);
         }
     }
 }
--- symtable.h
+++ symtable.h
@@ -7,20 +7,52 @@
 */
  #include <stdbool.h>
 
-// Uzel
-typedef struct btree_node {
+// Function struct
+
+typedef struct btree_function {
   char ident;           // identifikátor
   int order;            // pořadí
   char value;           // řetězec znaků/hodnota
-  struct btree_node *left;      // ukazatel na levý prvek
-  struct btree_node *right;     // ukazatel na pravý prvek
-} btree_node_t;
+  struct btree_function *left;      // ukazatel na levý prvek
+  struct btree_function *right;     // ukazatel na pravý prvek
+} btree_function_t;
 
-// Funkce
+void btree_function_init(btree_function_t **tree);
+void btree_function_dispose(btree_function_t **tree);
+void btree_function_add(btree_function_t **tree, int order, char ident, char value);
+bool btree_function_search(btree_function_t *tree, int order, char ident, char value);
 
-void btree_init(btree_node_t **tree);
-void btree_dispose(btree_node_t **tree);
 
-void btree_add(btree_node_t **tree, int order, char ident, char value);
 
-bool btree_search(btree_node_t *tree, int order, char ident, char value);
+// Variable struct
+
+typedef struct btree_variable {
+  char ident;           // identifikátor
+  int order;            // pořadí
+  char value;           // řetězec znaků/hodnota
+  struct btree_variable *left;      // ukazatel na levý prvek
+  struct btree_variable *right;     // ukazatel na pravý prvek
+} btree_variable_t;
+
+void btree_variable_init(btree_variable_t **tree);
+void btree_variable_dispose(btree_variable_t **tree);
+void btree_variable_add(btree_variable_t **tree, int order, char ident, char value);
+bool btree_variable_search(btree_variable_t *tree, int order, char ident, char value);
+
+
+
+// Program
+
+typedef struct btree_program {
+  char ident;           // identifikátor
+  int order;            // pořadí
+  char value;           // řetězec znaků/hodnota
+  struct btree_program *left;      // ukazatel na levý prvek
+  struct btree_program *right;     // ukazatel na pravý prvek
+} btree_program_t;
+
+void btree_program_init(btree_program_t **tree);
+void btree_program_dispose(btree_program_t **tree);
+void btree_program_add(btree_program_t **tree, int order, char ident, char value);
+bool btree_program_search(btree_program_t *tree, int order, char ident, char value);
+
